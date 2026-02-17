5193f44016f6832735953c3d55f19f7b17b8c9d4
KiranHundal <hundalkiran6@gmail.com>
2026-02-16 19:56:11 -0800

    refactor: clean up controller return types

--- src/tree/tree.controller.ts
+++ src/tree/tree.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TreeService } from './tree.service';
 import { CreateNodeDto } from './dto/create-node.dto';
-import { TreeNode } from './interface/tree-node.interface';
+import type { TreeNode } from './interface/tree-node.interface';
 
 @Controller('tree')
 export class TreeController {
   constructor(private readonly treeService: TreeService) {}
 
   @Get()
-  findAll(): Promise<TreeNode[]> {
-    return Promise.resolve(this.treeService.findAll());
+  findAll(): TreeNode[] {
+    return this.treeService.findAll();
   }
 
   @Post()
-  create(@Body() createNodeDto: CreateNodeDto): Promise<TreeNode> {
-    return Promise.resolve(this.treeService.create(createNodeDto));
+  create(@Body() createNodeDto: CreateNodeDto): TreeNode {
+    return this.treeService.create(createNodeDto);
   }
 }
